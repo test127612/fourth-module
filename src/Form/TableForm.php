6d1d82ec95cb1f7ba0cc40cfa31dcad7f5a80c39
Nastia <nastia.nadashkevich@gmail.com>
2022-06-14 10:20:24 +0300

    add table and buttons

--- src/Form/TableForm.php
+++ src/Form/TableForm.php
@@ -19,14 +19,57 @@ class TableForm extends FormBase {
    * {@inheritDoc}
    */
   public function getFormId() {
-    return 'table';
+    return 'table_form';
   }
 
   /**
    * {@inheritDoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form["#attached"]["library"][] = "may/global";
+    $form['add_year'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Add Year'),
+    ];
+
+    $header = [
+      'year' => t('Year'),
+      'jan' => t('Jan'),
+      'feb' => t('Feb'),
+      'mar' => t('Mar'),
+      'q1' => t('Q1'),
+      'apr' => t('Apr'),
+      'may' => t('May'),
+      'jun' => t('Jun'),
+      'q2' => t('Q2'),
+      'jul' => t('Jul'),
+      'aug' => t('Aug'),
+      'sep' => t('Sep'),
+      'q3' => t('Q3'),
+      'oct' => t('Oct'),
+      'nov' => t('Nov'),
+      'dec' => t('Dec'),
+      'q4' => t('Q4'),
+      'ytd' => t('YTD'),
+    ];
+
+    $form['table'] = [
+      '#type' => 'tableselect',
+      '#header' => $header,
+      '#empty' => t('Nothing found'),
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    $form['add_table'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Add Table'),
+    ];
+
+    $form['#attached']['library'][] = 'may/global';
+
     return $form;
   }
 
